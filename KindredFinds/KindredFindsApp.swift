ad51e703a145eb6e3ca45dfd51dc1255126bbe6a
Natanael06 <Natanaeldmedina@gmail.com>
2025-04-12 13:16:12 -0400

    Initial Commit

---
+++ KindredFinds/KindredFindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindredFindsApp.swift
+//  KindredFinds
+//
+//  Created by NATANAEL  MEDINA  on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct KindredFindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KindredFindsTests/KindredFindsTests.swift
@@ -0,0 +1,17 @@
+//
+//  KindredFindsTests.swift
+//  KindredFindsTests
+//
+//  Created by NATANAEL  MEDINA  on 4/12/25.
+//
+
+import Testing
+@testable import KindredFinds
+
+struct KindredFindsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
